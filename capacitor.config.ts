c6173c907ed8e6f2ac142981d08ddbf4e03067e7
Robbie Carlton <robbie.carlton@gmail.com>
2021-12-03 08:49:51 -0700

    update package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'capacitor-test',
+  appName: 'web-sdk-ionic-demo',
   webDir: 'build',
   bundledWebRuntime: false
 };
